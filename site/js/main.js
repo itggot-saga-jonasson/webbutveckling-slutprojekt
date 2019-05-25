0569606388c0b1b3d744f63c4746967aeb1c8cf7
itggot-saga-jonasson <saga.jonasson@elev.ga.ntig.se>
2019-05-25 18:52:41 +0200

    created a functioning info menu for mobile

--- site/js/main.js
+++ site/js/main.js
@@ -1,35 +1,43 @@
-function mobileDescription(){
-    // var x = document.getElementsByClassName("menu")[0];
-    // var y = document.getElementsByClassName("cover")[0];
-    // if(x.className === "menu"){
-    //     x.className += " appear";
-    //     y.className += " tadaa";
-    // }
-    console.log("yup it works")
+function mobileDescription(media_class){
+    popup = document.getElementsByClassName("popup_descr")[0]
+    cover = document.getElementsByClassName("cover")[0]
+
+    if (window.matchMedia("(max-width: 500px)").matches) {
+        if(cover.className === "cover"){
+            popup.className += " visible";
+            cover.className += " visible";
+
+            popup.innerHTML = '<button class="back" onclick=noMoreMobileDescription()>X</button>' + document.getElementsByClassName("description " + media_class)[0].innerHTML + '<a href="html/' + media_class + '.html"><button class="watch"><h2>Watch it now!</h2></button></a>'
+        } else if(cover.className === "cover invisible"){
+            popup.classList.replace("invisible", "visible");
+            cover.classList.replace("invisible", "visible");
+
+            popup.innerHTML = '<button class="back" onclick=noMoreMobileDescription()>X</button>' + document.getElementsByClassName("description " + media_class)[0].innerHTML + '<a href="html/' + media_class + '.html"><button class="watch"><h2>Watch it now!</h2></button></a>'
+        }
+    } 
+    
 }
 
-// function bringBackMenu(){
-//     var x = document.getElementsByClassName("menu")[0];
-//     var y = document.getElementsByClassName("cover")[0];
-//     if (x.className === "menu appear"){
-//         x.classList.remove("appear");
-//         y.classList.remove("tadaa");
-//     }
-// }
+function noMoreMobileDescription(){
+    if (document.getElementsByClassName("cover")[0].className === "cover visible"){
+        document.getElementsByClassName("cover")[0].classList.replace("visible", "invisible");
+        document.getElementsByClassName("popup_descr")[0].classList.replace("visible", "invisible");
+        
+        document.getElementsByClassName("popup_descr")[0].innerHTML = ""
+    }
+}
 
 function screenSize(){
     x = document.getElementsByClassName("video_path").length
     i = 0
 
     if (window.matchMedia("(min-width: 500px)").matches) {
-        console.log("Desktop mode");
         while(i < x){
             link = document.getElementsByClassName("video_path")[i].id;
             document.getElementsByClassName("video_path")[i].href = "html/" + link
             i+=1
         }
     } else {
-        console.log("Mobile mode");
         while(i < x){
             document.getElementsByClassName("video_path")[i].href = "javascript: void(0)";
             i+=1;
